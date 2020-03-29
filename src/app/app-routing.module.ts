f8cd9f4935308479e124507694f55e46e39dbb2a
Prahalad Belavadi <test@ALIPL0232.local>
2020-03-29 08:15:10 +0530

    add admin portal; Need to add functionality to delete volunteers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LandingComponent } from './pages/landing/landing.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SettingsComponent } from './pages/settings/settings.component';
 import { InventoryComponent } from './pages/inventory/inventory.component';
+import { PortalComponent } from './pages/portal/portal.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'login', component: LoginComponent },
   { path: 'settings', component: SettingsComponent },
-  { path: 'inventory', component: InventoryComponent }
+  { path: 'inventory', component: InventoryComponent },
+  { path: 'portal', component: PortalComponent }
 ];
 
 @NgModule({
