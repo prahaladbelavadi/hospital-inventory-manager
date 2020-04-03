87e8750708a0978bdc036d1d2f27cbe9eca57556
Prahalad Belavadi <test@ALIPL0232.local>
2020-04-03 07:43:06 +0530

    split into pages-components to reuse components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,10 @@ import { LoginComponent } from './pages/login/login.component';
 import { SettingsComponent } from './pages/settings/settings.component';
 import { InventoryComponent } from './pages/inventory/inventory.component';
 import { PortalComponent } from './pages/portal/portal.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, LandingComponent, LoginComponent, SettingsComponent, InventoryComponent, PortalComponent],
+  declarations: [AppComponent, LandingComponent, LoginComponent, SettingsComponent, InventoryComponent, PortalComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent]
